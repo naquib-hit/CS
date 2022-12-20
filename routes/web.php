fab093aa9b4ad17cdcea91a1d54fda8b9eb58306
naquib-hit <naquib@hitcorporation.com>
2022-12-20 16:44:02 +0700

    Add Migrations 20221220

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([
+    'registration' => false
+]);
+
+Route::resource('products', ProductController::class);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
