b2b13f604aea77590dd0513b872d72259dd5d272
Muhammad Naquib <76518349+naquib-hit@users.noreply.github.com>
2023-01-09 07:51:11 +0700

    1

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -42,19 +42,19 @@
                         </div>
                         <div class="card-body">
                             <div class="input-group input-group-outline">
-                                <label class="form-label">{{ __('invoice.form.fields.no') }}</label>
+                                <label class="form-label">{{ __('invoice.form.fields.no') }} <span class="text-danger">*</span></label>
                                 <input type="text" class="form-control" name="invoice_no">
                             </div>
                             <div class="input-group input-group-outline mt-3">
-                                <label class="form-label">{{ __('invoice.form.fields.customer') }}</label>
+                                <label class="form-label">{{ __('invoice.form.fields.customer') }} <span class="text-danger">*</span></label>
                                 <input type="text" class="form-control" name="invoice_customer" id="customer">
                             </div>
                             <div class="input-group input-group-outline mt-3">
-                                <label class="form-label">{{ __('invoice.form.fields.date') }}</label>
+                                <label class="form-label">{{ __('invoice.form.fields.date') }} <span class="text-danger">*</span></label>
                                 <input type="date" class="form-control" name="invoice_dates">
                             </div>
                             <div class="input-group input-group-outline mt-3">
-                                <label class="form-label">{{ __('invoice.form.fields.due_date') }}</label>
+                                <label class="form-label">{{ __('invoice.form.fields.due_date') }}<span class="text-danger">*</span></label>
                                 <input type="date" class="form-control" name="invoice_due">
                             </div>
                         </div>
@@ -86,7 +86,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-lg-8">
+                <div class="col-12 col-lg-4">
                     <div class="card fadeIn3 fadeInBottom">
                         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                             <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1 d-flex flex-nowrap align-items-center">
@@ -101,32 +101,35 @@
                         </div>
                         <div class="card-body" id="items-container">
                             <div class="row align-items-baseline">
-                                <div class="col-12 col-md-8">
+                                <div class="col-12 col-md-6 pe-1">
                                     <div class="input-group input-group-outline mt-3">
-                                        <label class="form-label"></label>
-                                        <input type="text" class="form-control item-name" placeholder="{{ __('invoice.form.fields.item') }}" name="invoice_item[0][name]">
+                                        <label class="form-label">{{ __('invoice.form.fields.item') }}<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control item-name" name="invoice_item[0][name]">
                                     </div>
                                     
                                 </div>
-                                <div class="col-12 col-md-2">
+                                <div class="col-12 col-md-4 px-1">
                                     <div class="input-group input-group-outline mt-3">
-                                        <label class="form-label"></label>
-                                        <input type="number" min="0" step="0.01" class="form-control item-total" placeholder="{{ __('invoice.form.fields.total') }}" name="invoice_item[0][total]">
+                                        <label class="form-label">{{ __('invoice.form.fields.total') }}<span class="text-danger">*</span></label>
+                                        <input type="number" min="0" step="0.01" class="form-control item-total"  name="invoice_item[0][total]">
                                     </div>
                                 </div>
-                                <div class="col-12 col-md-2">
-                                    <button type="button" class="btn btn-circle btn-danger m-0 p-0 clear-row"><i class="fas fa-trash font-reset"></i></button>
+                                <div class="col-12 col-md-2 ps-1">
+                                    <button type="button" class="btn btn-circle btn-danger m-0 p-0 clear-row" onclick="deleteItemRow(event)"><i class="fas fa-trash font-reset"></i></button>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    
+                </div>
+                <div class="col">
                     <!-- PAJAK -->
-                    <div class="card fadeIn3 fadeInBottom mt-5">
+                    <div class="card fadeIn3 fadeInBottom">
                         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                             <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1 d-flex flex-nowrap align-items-center">
                               <h4 class="text-white font-weight-bolder ms-3 my-0">{{ __('invoice.form.fields.tax') }}</h4>
                               <div class="ms-auto me-2">
-                                <button type="button" class="btn btn-sm btn-primary mb-0" id="add-item">
+                                <button type="button" class="btn btn-sm btn-primary mb-0" id="add-tax">
                                     <span class="font-weight-bolder me-1">+</span>
                                     {{ __('template.toolbar.add') }}
                                 </button>
@@ -212,24 +215,30 @@ import { Autocomplete } from "{{ asset('vendor/autocomplete/autocomplete.js') }}
         row.classList.add('row', 'mt-3', 'align-items-baseline');
         // col-left 
         const colLeft = document.createElement('div');
-        colLeft.classList.add('col-12', 'col-md-8');
+        colLeft.classList.add('col-12', 'col-md-6', 'pe-1');
         // col-middle
         const colMiddle = document.createElement('div');
-        colMiddle.classList.add('col-12', 'col-md-2');
+        colMiddle.classList.add('col-12', 'col-md-4','px-1');
         // col-right
         const colRight = document.createElement('div');
-        colRight.classList.add('col-12', 'col-md-2');
+        colRight.classList.add('col-12', 'col-md-2', 'ps-1');
         // input-group-left
         const inputGroupLeft = document.createElement('div');
         inputGroupLeft.classList.add('input-group', 'input-group-outline');
         // input-group-middlw (sengaja beda biar classListNya beda)
         const inputGroupMiddle = document.createElement('div');
         inputGroupMiddle.classList.add('input-group', 'input-group-outline');
-        // clear-row (sengaja beda biar classListNya beda)
+        // clear-row 
         const clearRow = document.createElement('button');
         clearRow.type = 'button';
-        clearRow.onclick = async e => await deleteItemRow(e);
+        clearRow.onclick = e => deleteItemRow(e);
         clearRow.classList.add('btn', 'btn-circle', 'btn-danger', 'm-0', 'p-0', 'clear-row');
+        // label buat text
+        const labelLeft = document.createElement('label');
+        labelLeft.classList.add('form-label');
+        // label buat number
+        const labelMiddle = document.createElement('label');
+        labelMiddle.classList.add('form-label');
        // input text
         const inputText = document.createElement('input');
         inputText.type = 'text';
@@ -244,19 +253,25 @@ import { Autocomplete } from "{{ asset('vendor/autocomplete/autocomplete.js') }}
         faTrash.classList.add('fas', 'fa-trash', 'font-reset');
 
         // LEFT
-        inputText.placeholder = "{{ __('invoice.form.fields.item') }}";
+        labelLeft.innerHTML = "{{ __('invoice.form.fields.item') }} <span class=\"text-danger\">*</span>";
         inputText.name = "invoice_items["+ (+lastIndex + 1) +"][name]";
-        //inputGroupLeft.appendChild(labelLeft);
+        inputText.onfocus = async e => await inputOnFocus(e);
+        inputText.onblur = async e => await inputOnFocusOut(e);
+        inputText.onkeyup = async e => await inputOnKeyup(e);
+        inputGroupLeft.appendChild(labelLeft);
         inputGroupLeft.appendChild(inputText);
         colLeft.appendChild(inputGroupLeft);
         row.appendChild(colLeft);
         // MIDDLE
-        //labelRight.innerText = '';
-        inputNumber.placeholder = "{{ __('invoice.form.fields.total') }}";
+        //labelMiddle.innerText = '';
+        labelMiddle.innerHTML = "{{ __('invoice.form.fields.total') }} <span class=\"text-danger\">*</span>";
         inputNumber.name= "invoice_items["+ (+lastIndex + 1) +"][total]";
+        inputNumber.onfocus = async e => await inputOnFocus(e);
+        inputNumber.onblur = async e => await inputOnFocusOut(e);
+        inputNumber.onkeyup = async e => await inputOnKeyup(e);
         inputNumber.min = 0;
         inputNumber.step = 0.01;
-        //inputGroupRight.appendChild(labelRight);
+        inputGroupMiddle.appendChild(labelMiddle);
         inputGroupMiddle.appendChild(inputNumber);
         colMiddle.appendChild(inputGroupMiddle);
         row.appendChild(colMiddle);
@@ -267,12 +282,117 @@ import { Autocomplete } from "{{ asset('vendor/autocomplete/autocomplete.js') }}
 
         return row;
     }
+
+    // tax group
+    const taxContainer = document.getElementById('tax-container'),
+          btnAddTax = document.getElementById('add-tax');
+    let elemTaxIndex = 0,
+        lastTaxIndex = document.getElementsByClassName('tax-name').length - 1;
+    
+    const createTax = () => {
+        // new row
+        const row = document.createElement('div');
+        row.classList.add('row', 'mt-3', 'align-items-baseline');
+        // col-left 
+        const colLeft = document.createElement('div');
+        colLeft.classList.add('col-12', 'col-md-6', 'pe-1');
+        // col-middle
+        const colMiddle = document.createElement('div');
+        colMiddle.classList.add('col-12', 'col-md-4','px-1');
+        // col-right
+        const colRight = document.createElement('div');
+        colRight.classList.add('col-12', 'col-md-2', 'ps-1');
+        // input-group-left
+        const inputGroupLeft = document.createElement('div');
+        inputGroupLeft.classList.add('input-group', 'input-group-outline');
+        // input-group-middlw (sengaja beda biar classListNya beda)
+        const inputGroupMiddle = document.createElement('div');
+        inputGroupMiddle.classList.add('input-group', 'input-group-outline');
+        // clear-row 
+        const clearRow = document.createElement('button');
+        clearRow.type = 'button';
+        clearRow.onclick = e => deleteItemRow(e);
+        clearRow.classList.add('btn', 'btn-circle', 'btn-danger', 'm-0', 'p-0', 'clear-row');
+        // label buat text
+        const labelLeft = document.createElement('label');
+        labelLeft.classList.add('form-label');
+        // label buat number
+        const labelMiddle = document.createElement('label');
+        labelMiddle.classList.add('form-label');
+       // input text
+        const inputText = document.createElement('input');
+        inputText.type = 'text';
+        inputText.classList.add('form-control', 'tax-name');
+        // input number
+        const inputNumber = document.createElement('input');
+        inputNumber.type = 'number';
+        inputNumber.classList.add('form-control');
+        // trash icon
+        const faTrash = document.createElement('i');
+        faTrash.onclick = () => false;
+        faTrash.classList.add('fas', 'fa-trash', 'font-reset');
+
+        // LEFT
+        labelLeft.innerText = "{{ __('invoice.form.fields.tax') }}";
+        inputText.name = "invoice_items["+ (+lastTaxIndex + 1) +"][name]";
+        inputText.onfocus = async e => await inputOnFocus(e);
+        inputText.onblur = async e => await inputOnFocusOut(e);
+        inputText.onkeyup = async e => await inputOnKeyup(e);
+        inputGroupLeft.appendChild(labelLeft);
+        inputGroupLeft.appendChild(inputText);
+        colLeft.appendChild(inputGroupLeft);
+        row.appendChild(colLeft);
+        // MIDDLE
+        //labelMiddle.innerText = '';
+        labelMiddle.innerText = "{{ __('invoice.form.fields.total') }}";
+        inputNumber.name= "invoice_items["+ (+lastIndex + 1) +"][total]";
+        inputNumber.onfocus = async e => await inputOnFocus(e);
+        inputNumber.onblur = async e => await inputOnFocusOut(e);
+        inputNumber.onkeyup = async e => await inputOnKeyup(e);
+        inputNumber.min = 0;
+        inputNumber.step = 0.01;
+        inputGroupMiddle.appendChild(labelMiddle);
+        inputGroupMiddle.appendChild(inputNumber);
+        colMiddle.appendChild(inputGroupMiddle);
+        row.appendChild(colMiddle);
+        // RIGHT
+        clearRow.appendChild(faTrash);
+        colRight.appendChild(clearRow);
+        row.appendChild(colRight);
+
+        return row;
+    }
+    //end tax group
+
     // check if window is changed
-    const deleteItemRow = async e => {
+    const deleteItemRow = e => {
         e = e || window.event;
         e.stopPropagation();
         const row = e.target.parentNode.closest('div.row');
         row.remove();
+    }
+
+    const inputOnFocus = async e => {
+        e = e || window.event;
+        e.srcElement.parentElement.classList.add('is-filled');
+    }
+
+    const inputOnKeyup = async e => {
+        e = e || window.event;
+        const el = e.srcElement;
+        const parent = el.parentElement;
+
+        if(el.value)
+            parent.classList.add('is-filled');
+        else
+            parent.classList.remove('is-filled');
+    }
+
+    const inputOnFocusOut = async e =>  {
+        e = e || window.event;
+        console.log(e.target);
+        if(!e.target.value)
+            e.target.parentElement.classList.remove('is-filled');
     }
     //observer.disconnect();
     // End Items Group
@@ -283,22 +403,14 @@ import { Autocomplete } from "{{ asset('vendor/autocomplete/autocomplete.js') }}
         btnAddItem.addEventListener('click', e => {
             e.preventDefault();
             lastIndex = document.getElementsByClassName('item-name').length - 1;
-
             itemContainer.appendChild(createItem());
-
-           
         });
 
-       
-
-        document.querySelectorAll('.clear-row').forEach(item => {
-            console.log(item);
-            item.addEventListener('click', e => {
-                e.stopPropagation();
-
-                console.log(e.target.parentNode.closest('.row'))
-            }, false);
-        });
+       btnAddTax.addEventListener('click', e => {
+            e.preventDefault();
+            lastIndex = document.getElementsByClassName('tax-name').length - 1;
+            taxContainer.appendChild(createTax());
+       });
 
     })();
 </script>
