6ccc4fc8d9a8c7d734d4a80b165e44e8dd5df118
naquib-hit <naquib@hitcorporation.com>
2023-01-17 17:28:42 +0700

    Commit-20230117 | Add Addtional Field Module

---
+++ database/migrations/2023_01_17_102542_create_additional_fields_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdditionalFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('additional_fields', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->unique()->constrained('invoices')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('field_name', 200);
+            $table->integer('field_value');
+            $table->string('unit', 100);
+            $table->string('operation', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('additional_fields');
+    }
+}
